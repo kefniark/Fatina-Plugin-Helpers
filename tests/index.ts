5b7a5fb525a30c5b2c51bfa3eea12a17fea1f6f3
Kevin Destrem <kevin@destrem.net>
2017-06-22 02:43:12 +0900

    add more tests

--- tests/index.ts
+++ tests/index.ts
@@ -62,6 +62,7 @@ test('Test Move Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.position.x, 3, 'Check the object moved properly');
 
+	t.notOk(obj.MoveToRel(2, 2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -74,6 +75,7 @@ test('Test Move X Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.position.x, 3, 'Check the object moved properly');
 
+	t.notOk(obj.MoveYToRel(2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -86,6 +88,7 @@ test('Test Move Y Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.position.y, 3, 'Check the object moved properly');
 
+	t.notOk(obj.MoveYToRel(2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -99,6 +102,7 @@ test('Test PunchPosition', function (t: any) {
 	t.equal(obj.position.x, 0, 'Check the object went back to original position');
 	t.ok(tween.IsFinished(), 'Check the tween is finished');
 
+	t.notOk(obj.PunchPosition(2, 2, 3, 5, false).IsRunning());
 	t.end();
 });
 
@@ -112,6 +116,7 @@ test('Test Shake', function (t: any) {
 	t.equal(obj.position.x, 0, 'Check the object went back to original position');
 	t.ok(tween.IsFinished(), 'Check the tween is finished');
 
+	t.notOk(obj.Shake(2, 2, 3, 5, false).IsRunning());
 	t.end();
 });
 
@@ -135,6 +140,7 @@ test('Test RotateRelative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.rotation, 2 , 'Check the object rotated properly');
 
+	t.notOk(obj.RotateToRel(1, 5, false).IsRunning());
 	t.end();
 });
 
@@ -162,6 +168,7 @@ test('Test Rotate Deg Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.rotation, Math.PI * 2, 'Check the object rotated properly');
 
+	t.notOk(obj.RotateDegToRel(90, 5, false).IsRunning());
 	t.end();
 });
 
@@ -175,6 +182,7 @@ test('Test PunchRotate', function (t: any) {
 	t.equal(obj.rotation, 0, 'Check the object went back to original rotation');
 	t.ok(tween.IsFinished(), 'Check the tween is finished');
 
+	t.notOk(obj.PunchRotate(2, 3, 5, false).IsRunning());
 	t.end();
 });
 
@@ -186,6 +194,7 @@ test('Test Fade', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.alpha, 0.5, 'Check the object alpha changed properly');
 
+	t.notOk(obj.FadeTo(0.5, 5, false).IsRunning());
 	t.end();
 });
 
@@ -230,6 +239,7 @@ test('Test Scale Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.scale.x, 3, 'Check the object scaled changed properly');
 
+	t.notOk(obj.ScaleToRel(2, 2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -241,6 +251,7 @@ test('Test Scale X Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.scale.x, 3, 'Check the object scaled changed properly');
 
+	t.notOk(obj.ScaleXToRel(2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -252,6 +263,7 @@ test('Test Scale Y Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.scale.y, 3, 'Check the object scaled changed properly');
 
+	t.notOk(obj.ScaleYToRel(2, 5, false).IsRunning());
 	t.end();
 });
 
@@ -265,6 +277,7 @@ test('Test PunchScale', function (t: any) {
 	t.equal(obj.scale.x, 1, 'Check the object went back to original position');
 	t.ok(tween.IsFinished(), 'Check the tween is finished');
 
+	t.notOk(obj.PunchScale(2, 2, 3, 5, false).IsRunning());
 	t.end();
 });
 
@@ -288,6 +301,7 @@ test('Test Color Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.hue.r, 1.5, 'Check the object color changed properly');
 
+	t.notOk(obj.ColorToRel(1, 1, 1, 5, false).IsRunning());
 	t.end();
 });
 
@@ -311,6 +325,7 @@ test('Test Color Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.hue.r, 1.5, 'Check the object color changed properly');
 
+	t.notOk(obj.ColorRGBToRel('0xFFFFFF', 5, false).IsRunning());
 	t.end();
 });
 
@@ -324,6 +339,7 @@ test('Test PunchColor', function (t: any) {
 	t.equal(obj.hue.r, 0, 'Check the object went back to original position');
 	t.ok(tween.IsFinished(), 'Check the tween is finished');
 
+	t.notOk(obj.PunchColor(1, 1, 1, 3, 5, false).IsRunning());
 	t.end();
 });
 
@@ -347,6 +363,7 @@ test('Test Tint Color Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.tint, 10833914, 'Check the object color changed properly');
 
+	t.notOk(obj.ColorToRel(0.25, 0.25, 0.25, 5, false).IsRunning());
 	t.end();
 });
 
@@ -370,6 +387,7 @@ test('Test Tint Color Relative', function (t: any) {
 	Fatina.Update(5);
 	t.equal(obj.tint, 8947814, 'Check the object color changed properly');
 
+	t.notOk(obj.ColorRGBToRel('#222200', 5, false).IsRunning());
 	t.end();
 });
 
